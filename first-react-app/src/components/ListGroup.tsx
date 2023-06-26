c77406b054c45a8f283689ef03d04921ef3da36e
Rahul Sanjay Mandviya <Rahulmandviya@gmail.com>
2023-06-26 17:15:30 +0530

    Events-state-props

--- first-react-app/src/components/ListGroup.tsx
+++ first-react-app/src/components/ListGroup.tsx
@@ -1,16 +1,45 @@
-function ListGroup() {
-  let items = ["Mumbai", "Pune", "Bhopal", "New Delhi", "Chennai"];
-  items = [];
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
+// { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // items = [];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Event Handler
+  //const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* {items.length === 0 ? <p>No item found</p> : null} */}
-      {items.length === 0 && <p>No item found</p>} // common technique used by
-      react developers
+      {items.length === 0 && <p>No item found</p>}{" "}
+      {/* common technique used by react developers */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
