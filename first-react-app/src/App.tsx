d9d41ec7c3d8e4094a0ae9f9781025526fb0dfdd
Rahul Sanjay Mandviya <Rahulmandviya@gmail.com>
2023-07-07 13:05:03 +0530

    useEffect-complete

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -63,13 +63,51 @@
 // export default App;
 
 //useEffect tutorial - https://youtu.be/0ZJgIjIuY7U?list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const [resourceType, setResourceType] = useState("posts");
+  // const [items, setItems] = useState([]);
+
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
+  // }, [resourceType]);
+
+  useEffect(() => {
+    console.log("resource changed");
+
+    return () => {
+      console.log("return from resource changed");
+    };
+  }, [resourceType]);
 
   return (
     <>
+      {/* <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{[resourceType]}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })} */}
+      {/* <div>{windowWidth}</div> */}
       <div>
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("users")}>Users</button>
